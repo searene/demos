75d84cbcf36da1230b3ea1acb5a5a372abb1d598
searene <mycityofsky@gmail.com>
2016-01-06 17:23:19 +0800

    Create hotspot.sh

---
+++ hotspot.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# This file is used to fire wifi signal from PC, working properly on ubuntu 15.10
+
+trap ctrl_c INT
+function ctrl_c() {
+	echo "** Trapped CTRL-C"
+	# Stop
+	# Disable NAT
+	sudo iptables -D POSTROUTING -t nat -o eth0 -j MASQUERADE
+	# Disable routing
+	sudo sysctl net.ipv4.ip_forward=0
+	# Disable DHCP/DNS server
+	sudo service dnsmasq stop
+	sudo service hostapd stop
+	sudo /etc/init.d/network-manager restart
+	sleep 3
+	sudo nmcli r wifi on
+}
+# Start
+# Configure IP address for WLAN
+sudo nmcli r wifi on
+sudo /etc/init.d/network-manager restart
+sleep 3
+sudo nmcli r wifi off
+sudo rfkill unblock wlan
+sleep 1
+sudo ifconfig wlan0 192.168.150.1/24 up
+# Start DHCP/DNS server
+sudo service dnsmasq restart
+# Enable routing
+sudo sysctl net.ipv4.ip_forward=1
+# Enable NAT
+sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+# Run access point daemon
+sudo hostapd /etc/ap-hotspot.conf

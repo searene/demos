1406f3da3cfc201b59b6666722512a26dc25b9be
Joey <hainyu@cisco.com>
2016-09-15 18:39:10 +0800

    without popup

--- send_to_kindle/send_to_kindle_chrome/event.js
+++ send_to_kindle/send_to_kindle_chrome/event.js
@@ -1,4 +1,39 @@
-// This function is called onload in the popup code
+// var HOST = 'http://searene.party';
+var HOST = 'http://searene.party';
+var PORT = '3000';
+
+function upload(plain, html) {
+  $.ajax({
+    method: 'POST',
+    url: HOST + ':' + PORT + '/upload',
+    data: {plain: plain, html: html}
+  });
+}
+
+function onPageDetailsReceived(details) {
+  // get html
+  var htmlCode = details.summary;
+  var sel = window.getSelection && window.getSelection();
+    if (sel && sel.rangeCount > 0) {
+      var range = sel.getRangeAt(0);
+      if (range) {
+          var div = document.createElement('div');
+          div.appendChild(range.cloneContents());
+          htmlCode = div.innerHTML;
+      }
+  }
+  chrome.tabs.executeScript( {
+    code: "window.getSelection().toString();"
+  }, function(selection) {
+
+    // get plain text
+    var plainText = selection[0];
+
+    // upload plain and html texts to server
+    upload(plainText, htmlCode);
+  });
+}
+
 function getPageDetails(callback) { 
 	// Inject the content script into the current page 
 	chrome.tabs.executeScript(null, { file: 'content.js' }); 
@@ -8,3 +43,8 @@ function getPageDetails(callback) {
 		callback(message); 
 	}); 
 };
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  getPageDetails(onPageDetailsReceived);
+});
+

c4b51b17dfb5097f984a38c98dfc98e0d6db8cb2
Joey <hainyu@cisco.com>
2016-09-15 18:42:01 +0800

    change font size in h.html

--- send_to_kindle/send_to_kindle_server/routes/index.js
+++ send_to_kindle/send_to_kindle_server/routes/index.js
@@ -24,20 +24,22 @@ router.post('/upload', function(req, res) {
   var plain = req.body.plain;
   var html = req.body.html;
   var plainFormat = '<html><head><link rel="stylesheet" type="text/css" href="stylesheets/style.css"></head><body><pre>{0}</pre></body></html>';
+        var htmlFormat = '<link rel="stylesheet" type="text/css" href="stylesheets/style_html.css">{0}';
 
   // location of p.html and h.html
   var pfile = path.join(__dirname, '..', 'public', 'p.html');
   var hfile = path.join(__dirname, '..', 'public', 'h.html');
 
-  // text that will be written into p.html
+  // text that will be written into p.html/h.html
   var pContent = plainFormat.format(plain);
+  var hContent = htmlFormat.format(html);
 
   fs.writeFile(pfile, pContent, function(err) {
     if(err) {
       return console.log(err);
     }
   });
-  fs.writeFile(hfile, html, function(err) {
+  fs.writeFile(hfile, hContent, function(err) {
     if(err) {
       return console.log(err);
     }
